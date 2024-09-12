8733c674f416bf9bac5f30c9bf2d9633357aa40b
Omar Faruk <omar.faruk@outlook.com>
2024-09-12 17:37:57 +0100

    Updated some instructions

--- src/pages/ChughtaiDownload.tsx
+++ src/pages/ChughtaiDownload.tsx
@@ -72,7 +72,7 @@ export default function ChughtaiDownload() {
       <Stack>
         <Title order={4}>Book Url</Title>
 
-        <Text size="md">This is the URL to the book you want to download. This page should show the book details. Once logged in, you should see the flip book on this page displaying the book. If you cannot see it, this book might not be supported for download using this tool.</Text>
+        <Text size="md">This is the URL to the book you want to download. This page should show the book details. Once logged in, you should see the flip book on this page displaying the book. If you cannot see book pages in your browser, this book might not be supported for download using this tool.</Text>
         
         <Title order={4}>Session Id</Title>
 
@@ -81,14 +81,15 @@ export default function ChughtaiDownload() {
         <Text size="md">Follow these steps to get the session id:</Text>
 
         <List>
-          <List.Item>Navigate to library website <a href="https://www.chughtailibrary.com/" target="_blank">https://www.chughtailibrary.com/</a>.</List.Item>
-          <List.Item>Open the book page that you want to download. If you can see the "Please click here to sign-in" button, as shown in the image below, please click it. </List.Item>
+          <List.Item>Navigate to library website <a href="https://www.chughtailibrary.com/" target="_blank">https://www.chughtailibrary.com/</a> in your browser.</List.Item>
+          <List.Item>Open the book page that you want to download. If you can see the "Please click here to sign-in" button, instead of book pages, please click it. </List.Item>
           <List.Item>Provide your login information and sign-in.</List.Item>
           <List.Item>You should be navigated back to book page and this time you should be able to see the book pages in the Book tab.</List.Item>
           <List.Item>If you cannot see the pages of the book after signing in, this book is not available to be downloaded using this toList.Item.</List.Item>
           <List.Item>Find the cookies for the library website. <a href="https://www.cookieyes.com/blog/how-to-check-cookies-on-your-website-manually/" target="_blank">See this link for guide for your browser</a>.</List.Item>
           <List.Item>You need value for the cookie with name PHPSESSID for the domain www.chughtailibrary.com</List.Item>
           <List.Item>Use the value as Session Id field  to download.</List.Item>
+          <List.Item>It is important that you do not close the library website while download is in progress as closing website would render the session id invalid and download may not work properly.</List.Item>
         </List>
 
         <Text size="md">Once download is started, please wait patiently while book is being download. Depending on number of pages in the book it can take a while to download.</Text>
